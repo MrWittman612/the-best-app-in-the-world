da871fc8c45c7491f62928af060a4852054567f5
1sfaccia1 <chad.m.wittman75@gmail.com>
2020-04-23 21:31:51 -0500

    create-react-app initial commit

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const app = express();
 
 const port = process.env.PORT || 8888;
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
 });
